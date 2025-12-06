062634fed352c97df2fb726cdd423f2583da6126
Philip Krag Nielsen <philipkragnielsen@gmail.com>
2025-12-06 10:39:18 +0100

    Solve Day 6 Part 1 (2025)

---
+++ 2025/day06/day06-test.rkt
@@ -0,0 +1,28 @@
+#lang racket
+
+(require "day06.rkt")
+
+(module+ test
+  (require rackunit rackunit/text-ui)
+
+  (define input (file->string "input.txt"))
+  (define sample "123 328  51 64\n 45 64  387 23\n  6 98  215 314\n*   +   *   +  ")
+  
+  (define suite
+    (test-suite "Cephalopod mathematics"
+      (test-eq? "Simple addition works"
+                (compute-total "3\n3\n+")
+                6)
+
+      (test-eq? "Simple multiplication works"
+                (compute-total "3\n3\n*")
+                9)
+                
+      (test-eq? "Can solve sample case for Part 1"
+                (compute-total sample)
+                4277556)
+
+      (test-eq? "Can find the correct result from input.txt for Part 1"
+                (compute-total input)
+                4412382293768)))
+  (run-tests suite))
---
+++ 2025/day06/day06.rkt
@@ -0,0 +1,56 @@
+#lang racket
+
+(require racket/list)
+
+(provide compute-total)
+
+(define (text->grid text)
+  (let* ([lines0 (regexp-split #px"\r?\n" text)]
+         [lines
+          (let loop ([rs (reverse lines0)])
+            (if (and (pair? rs) (string=? (car rs) "")) (loop (cdr rs)) (reverse rs)))]
+         [w (apply max 0 (map string-length lines))])
+    (map (λ (s) (string-append s (make-string (- w (string-length s)) #\space))) lines)))
+
+(define (column-ranges grid)
+  (let* ([w (string-length (car grid))]
+         [space-col? (λ (j) (for/and ([row grid]) (char=? (string-ref row j) #\space)))])
+    (let loop ([j 0] [in? #f] [start 0] [acc '()])
+      (cond
+        [(= j w) (reverse (if in? (cons (cons start w) acc) acc))]
+        [(space-col? j)
+         (loop (add1 j) #f start (if in? (cons (cons start j) acc) acc))]
+        [else (loop (add1 j) #t (if in? start j) acc)]))))
+
+(define (nums-by-rows grid start end)
+  (let* ([h (length grid)]
+         [top (take grid (sub1 h))]
+         [seg (λ (s) (substring s start end))])
+    (filter values
+            (for/list ([row top])
+              (let ([ds (regexp-replace* #px"\\D" (seg row) "")])
+                (and (positive? (string-length ds)) (string->number ds)))))
+    ))
+
+(define (op-in-block grid start end)
+  (let* ([bottom (list-ref grid (sub1 (length grid)))]
+         [seg (substring bottom start end)]
+         [m (regexp-match #rx"[+*]" seg)])
+    (unless m (error 'op-in-block "no operator in block ~a-~a" start end))
+    (string-ref (car m) 0)))
+
+(define (solve-with text nums-extractor)
+  (let ([grid (text->grid text)])
+    (if (null? grid) 0
+        (for/sum ([rg (column-ranges grid)])
+          (let* ([start (car rg)]
+                 [end   (cdr rg)]
+                 [nums  (nums-extractor grid start end)]
+                 [op    (op-in-block grid start end)])
+            (when (null? nums)
+              (error 'solve "block ~a-~a has no numbers" start end))
+            (if (char=? op #\+) (apply + nums) (apply * nums))))
+        )))
+
+(define (compute-total text)
+  (solve-with text nums-by-rows))
