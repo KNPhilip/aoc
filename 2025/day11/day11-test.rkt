587bf7e9c7a924b1d105c8b68d30e638ec7eacd2
Philip Krag Nielsen <philipkragnielsen@gmail.com>
2025-12-11 06:49:25 +0100

    Solve Day 11 Part 2 (2025)

--- 2025/day11/day11-test.rkt
+++ 2025/day11/day11-test.rkt
@@ -16,22 +16,37 @@
     "ggg: out"
     "hhh: ccc fff iii"
     "iii: out"))
+
+  (define sample2 '(
+    "svr: aaa bbb"
+    "aaa: fft"
+    "fft: ccc"
+    "bbb: tty"
+    "tty: ccc"
+    "ccc: ddd eee"
+    "ddd: hub"
+    "hub: fff"
+    "eee: dac"
+    "dac: fff"
+    "fff: ggg hhh"
+    "ggg: out"
+    "hhh: out"))
   
   (define input (file->lines "input.txt"))
 
   (define suite
     (test-suite "Reactor: Device attachments test suite"
                 
-      (test-case "Single direct connection you -> out"
+      (test-case "Part 1 - Single direct connection you -> out"
         (check-equal? (find-total-paths '("you: out" "out:")) 1))
 
-      (test-case "No path you -> out"
+      (test-case "Part 1 - No path you -> out"
         (check-equal? (find-total-paths '("you: aaa" "aaa: bbb" "bbb:" "out:")) 0))
 
-      (test-case "Multiple simple parallel paths"
+      (test-case "Part 1 - Multiple simple parallel paths"
         (check-equal? (find-total-paths '("you: a b" "a: out" "b: out" "out:")) 2))
 
-      (test-case "Single branching chain with one dead end"
+      (test-case "Part 1 - Single branching chain with one dead end"
         ;; you → a → out
         ;; you → b → c (dead end)
         (check-equal?
@@ -42,7 +57,7 @@
                              "out:"))
          1))
 
-      (test-case "Three-step linear path"
+      (test-case "Part 1 - Three-step linear path"
         (check-equal?
          (find-total-paths '("you: a"
                              "a: b"
@@ -54,5 +69,47 @@
         (check-equal? (find-total-paths sample) 5))
 
       (test-case "Can find the correct result from input.txt for Part 1"
-        (check-equal? (find-total-paths input) 746))))
+        (check-equal? (find-total-paths input) 746))
+
+      (test-case "Part 2 - no path to out"
+        (check-equal?
+         (find-total-paths-2 '("svr: a" "a: b" "b:" "dac:" "fft:" "out:")) 0))
+
+      (test-case "Part 2 - paths exist but missing dac or fft"
+        (check-equal?
+         (find-total-paths-2
+          '("svr: a b"
+            "a: out"
+            "b: out"
+            "dac:"
+            "fft:"
+            "out:"))
+         0))
+
+      (test-case "Part 2 - exactly one satisfying path"
+        (check-equal?
+         (find-total-paths-2
+          '("svr: a"
+            "a: fft"
+            "fft: dac"
+            "dac: out"
+            "out:"))
+         1))
+
+      (test-case "Part 2 - two valid paths"
+        (check-equal?
+         (find-total-paths-2
+          '("svr: x y"
+            "x: fft"
+            "y: fft"
+            "fft: dac"
+            "dac: out"
+            "out:"))
+         2))
+
+      (test-case "Can solve sample case for Part 2"
+        (check-equal? (find-total-paths-2 sample2) 2))
+
+      (test-case "Can find the correct result from input.txt for Part 2"
+        (check-equal? (find-total-paths-2 input) 370500293582760))))
   (run-tests suite))
--- 2025/day11/day11.rkt
+++ 2025/day11/day11.rkt
@@ -1,18 +1,21 @@
 #lang racket
 
-(provide find-total-paths)
+(provide find-total-paths
+         find-total-paths-2)
 
+;; Shared
+(define (parse-line line)
+  (let* ([parts (string-split line ":")]
+         [src   (string-trim (first parts))]
+         [outs  (if (> (length parts) 1)
+                    (map string-trim
+                         (string-split (second parts)))
+                         '())])
+    (cons src outs)))
+
+;; Part 1
 (define (find-total-paths lines)
-  (let* ([parse-line
-          (lambda (line)
-            (let* ([parts (string-split line ":")]
-                   [src   (string-trim (first parts))]
-                   [outs  (if (> (length parts) 1)
-                              (map string-trim
-                                   (string-split (second parts)))
-                              '())])
-              (cons src outs)))]
-         [edges     (map parse-line lines)]
+  (let* ([edges     (map parse-line lines)]
          [adjacency (for/hash ([p edges])
                       (values (car p) (cdr p)))])
     (letrec ([dfs
@@ -25,6 +28,32 @@
                      (for*/list ([n nexts]
                                  #:unless (set-member? visited n)
                                  [p (dfs n (set-add visited n))])
-                       (cons node p)))]))])
+                       (cons node p)))]
+                ))])
       (length (dfs "you" (set "you")))
     )))
+
+;; Part 2
+(define (find-total-paths-2 lines)
+  (let ([adjacency
+         (for/hash ([p (map parse-line lines)])
+           (values (car p) (cdr p)))]
+        [memo (make-hash)]
+        [DAC? 1]
+        [FFT? 2])
+    (define (dfs node mask)
+      (define newmask
+        (cond [(equal? node "dac") (bitwise-ior mask DAC?)]
+              [(equal? node "fft") (bitwise-ior mask FFT?)]
+              [else mask]))
+      (cond
+        [(equal? node "out")
+         (if (= newmask (bitwise-ior DAC? FFT?)) 1 0)]
+        [else
+         (define key (cons node newmask))
+         (hash-ref! memo key
+                    (lambda ()
+                      (for/sum ([n (hash-ref adjacency node '())])
+                        (dfs n newmask))))
+        ]))
+  (dfs "svr" 0)))
